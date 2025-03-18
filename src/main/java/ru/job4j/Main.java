115d504188cfd7a9daff9e73979eb3d2ebd602d9
as <bezotosniyau@yandex.ru>
2025-03-18 06:35:27 +0300

    Жизненный цикл сборки Gradle [#505351]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,7 @@
 package ru.job4j;
-
+// My first program
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world"); // print output
     }
 }
